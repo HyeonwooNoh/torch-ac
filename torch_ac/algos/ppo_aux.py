3ff579e2271c691f58fe2f8a55e755d03df2e5f2
Hyeonwoo Noh <hyeonwoonoh@gmail.com>
2019-05-28 07:04:14 +0900

    add auxiliary reward

--- torch_ac/algos/ppo_aux.py
+++ torch_ac/algos/ppo_aux.py
@@ -12,12 +12,19 @@ class PPOAuxAlgo(PPOAlgo):
     def __init__(self, envs, acmodel, num_frames_per_proc=None, discount=0.99, lr=7e-4, gae_lambda=0.95,
                  entropy_coef=0.01, value_loss_coef=0.5, aux_loss_coef=0.5, max_grad_norm=0.5, recurrence=4,
                  adam_eps=1e-5, clip_eps=0.2, epochs=4, batch_size=256, preprocess_obss=None,
-                 reshape_reward=None):
-        self.aux_loss_coef = aux_loss_coef
+                 reshape_reward=None, use_aux_reward=False, aux_reward_coef=0.1):
         super().__init__(envs, acmodel, num_frames_per_proc, discount, lr, gae_lambda,
                          entropy_coef, value_loss_coef, max_grad_norm, recurrence,
                          adam_eps, clip_eps, epochs, batch_size, preprocess_obss,
                          reshape_reward)
+
+        self.aux_loss_coef = aux_loss_coef
+        self.use_aux_reward = use_aux_reward
+        self.aux_reward_coef = aux_reward_coef
+
+        shape = (self.num_frames_per_proc, self.num_procs)
+        self.sample_entropies = torch.zeros(*shape, device=self.device)
+        self.prev_aux_logprobs = torch.zeros(*shape, device=self.device)
 
     def collect_experiences(self):
         """Collects rollouts and computes advantages.
@@ -45,14 +52,19 @@ class PPOAuxAlgo(PPOAlgo):
 
             preprocessed_obs = self.preprocess_obss(self.obs, device=self.device)
             with torch.no_grad():
+                masked_prev_action = self.prev_action * self.mask.int()
                 if self.acmodel.recurrent:
-                    dist, value, memory, _ = self.acmodel(
-                        preprocessed_obs, self.prev_action * self.mask.int(),
+                    dist, value, memory, auxdist = self.acmodel(
+                        preprocessed_obs, masked_prev_action,
                         self.memory * self.mask.unsqueeze(1))
                 else:
-                    dist, value, _ = self.acmodel(
-                        preprocessed_obs, self.prev_action * self.mask.int())
+                    dist, value, auxdist = self.acmodel(
+                        preprocessed_obs, masked_prev_action)
             action = dist.sample()
+            # Auxiliary reward is expected mutual information
+
+            prev_aux_logprob = auxdist.log_prob(masked_prev_action)
+            sample_entropy = -dist.log_prob(action)
 
             obs, reward, done, _ = self.env.step(action.cpu().numpy())
 
@@ -69,6 +81,9 @@ class PPOAuxAlgo(PPOAlgo):
             self.mask = 1 - torch.tensor(done, device=self.device, dtype=torch.float)
             self.actions[i] = action
             self.values[i] = value
+            self.sample_entropies[i] = sample_entropy
+            if i != 0:
+                self.prev_aux_logprobs[i-1] = prev_aux_logprob
             if self.reshape_reward is not None:
                 self.rewards[i] = torch.tensor([
                     self.reshape_reward(obs_, action_, reward_, done_)
@@ -99,17 +114,28 @@ class PPOAuxAlgo(PPOAlgo):
 
         preprocessed_obs = self.preprocess_obss(self.obs, device=self.device)
         with torch.no_grad():
+            masked_prev_action = self.prev_action * self.mask.int()
             if self.acmodel.recurrent:
-                _, next_value, _, _ = self.acmodel(
-                    preprocessed_obs, self.prev_action * self.mask.int(),
+                _, next_value, _, next_auxdist = self.acmodel(
+                    preprocessed_obs, masked_prev_action,
                     self.memory * self.mask.unsqueeze(1))
             else:
-                _, next_value, _ = self.acmodel(
-                    preprocessed_obs, self.prev_action * self.mask.int())
+                _, next_value, next_auxdist = self.acmodel(
+                    preprocessed_obs, masked_prev_action)
+
+        self.prev_aux_logprobs[self.num_frames_per_proc-1] = \
+            next_auxdist.log_prob(masked_prev_action)
 
         for i in reversed(range(self.num_frames_per_proc)):
             next_mask = self.masks[i+1] if i < self.num_frames_per_proc - 1 else self.mask
-            next_value = self.values[i+1] if i < self.num_frames_per_proc - 1 else next_value
+            if self.use_aux_reward:
+                if i < self.num_frames_per_proc - 1:
+                    next_value = self.values[i+1] + \
+                        self.aux_reward_coef * (self.sample_entropies[i+1] + self.prev_aux_logprobs[i+1])
+                else:
+                    next_value = next_value
+            else:
+                next_value = self.values[i+1] if i < self.num_frames_per_proc - 1 else next_value
             next_advantage = self.advantages[i+1] if i < self.num_frames_per_proc - 1 else 0
 
             delta = self.rewards[i] + self.discount * next_value * next_mask - self.values[i]
@@ -136,6 +162,8 @@ class PPOAuxAlgo(PPOAlgo):
         exps.action = self.actions.transpose(0, 1).reshape(-1)
         exps.prev_action = self.prev_actions.transpose(0, 1).reshape(-1)
         exps.value = self.values.transpose(0, 1).reshape(-1)
+        exps.sample_entropy = self.sample_entropies.transpose(0, 1).reshape(-1)
+        exps.prev_aux_logprob = self.prev_aux_logprobs.transpose(0, 1).reshape(-1)
         exps.reward = self.rewards.transpose(0, 1).reshape(-1)
         exps.advantage = self.advantages.transpose(0, 1).reshape(-1)
         exps.returnn = exps.value + exps.advantage
@@ -170,6 +198,8 @@ class PPOAuxAlgo(PPOAlgo):
             # Initialize log values
 
             log_entropies = []
+            log_sample_entropies = []
+            log_prev_aux_logprobs = []
             log_values = []
             log_policy_losses = []
             log_value_losses = []
@@ -180,6 +210,8 @@ class PPOAuxAlgo(PPOAlgo):
                 # Initialize batch values
 
                 batch_entropy = 0
+                batch_sample_entropy = 0
+                batch_prev_aux_logprob = 0
                 batch_value = 0
                 batch_policy_loss = 0
                 batch_value_loss = 0
@@ -225,6 +257,8 @@ class PPOAuxAlgo(PPOAlgo):
                     # Update batch values
 
                     batch_entropy += entropy.item()
+                    batch_sample_entropy += sb.sample_entropy.mean().item()
+                    batch_prev_aux_logprob += sb.prev_aux_logprob.mean().item()
                     batch_value += value.mean().item()
                     batch_policy_loss += policy_loss.item()
                     batch_value_loss += value_loss.item()
@@ -239,6 +273,8 @@ class PPOAuxAlgo(PPOAlgo):
                 # Update batch values
 
                 batch_entropy /= self.recurrence
+                batch_sample_entropy /= self.recurrence
+                batch_prev_aux_logprob /= self.recurrence
                 batch_value /= self.recurrence
                 batch_policy_loss /= self.recurrence
                 batch_value_loss /= self.recurrence
@@ -256,6 +292,8 @@ class PPOAuxAlgo(PPOAlgo):
                 # Update log values
 
                 log_entropies.append(batch_entropy)
+                log_sample_entropies.append(batch_sample_entropy)
+                log_prev_aux_logprobs.append(batch_prev_aux_logprob)
                 log_values.append(batch_value)
                 log_policy_losses.append(batch_policy_loss)
                 log_value_losses.append(batch_value_loss)
@@ -266,6 +304,8 @@ class PPOAuxAlgo(PPOAlgo):
 
         logs = {
             "entropy": numpy.mean(log_entropies),
+            "sample_entropy": numpy.mean(log_sample_entropies),
+            "prev_aux_logprob": numpy.mean(log_prev_aux_logprobs),
             "value": numpy.mean(log_values),
             "policy_loss": numpy.mean(log_policy_losses),
             "value_loss": numpy.mean(log_value_losses),
